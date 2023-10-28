39d4c294fc04dc61359d482ea306b68d47f47162
rayhan57 <rayhanb18@gmail.com>
2023-10-28 13:04:49 +0700

    add transition

--- app/components/ModalImage.jsx
+++ app/components/ModalImage.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { Image, Modal } from "react-bootstrap";
+import styles from "../page.module.css";
 
 const ModalImage = ({ show, setShow, item }) => {
   return (
     <>
-      <Modal show={show} onHide={() => setShow(false)} centered>
+      <Modal
+        className={styles.myModal}
+        show={show}
+        onHide={() => setShow(false)}
+        centered
+      >
         <Modal.Body>
           <Image src={item.url} alt={item.name} fluid />
         </Modal.Body>
